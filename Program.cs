959cd88f8d142db825ef38656ee23e5467f1f6c4
Monsterter <ibrahimsenc@gmail.com>
2023-10-17 17:57:20 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+int month = DateTime.Now.Month;
+
+switch (month)
+{
+    
+    case 10:
+        Console.WriteLine("Ekim ayındasınız");
+    break;
+    case 11:
+        Console.WriteLine("Kasım ayındasınız");
+    break;
+    case 12:
+        Console.WriteLine("Aralık ayındasınız");
+    break;
+
+    default:
+    Console.WriteLine("Yanlış veri girdiniz");
+    break;
+}
+switch (month)
+{
+    case 10:
+    case 11:
+    case 12:
+    Console.WriteLine("Kış ayındasınız");
+    break;
+    
+    default:
+
+    break;
+}
